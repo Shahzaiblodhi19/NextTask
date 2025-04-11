ab2eb4dd5ee8df3f33fa8f323886925395b60531
Shahzaib Lodhi <shahzaiblodhi2233@gmail.com>
2025-04-11 07:10:18 +0500

    first commit

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,7 @@
 // lib/supabaseClient.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = "https://sdscffnbhcvhwsbraqbc.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkc2NmZm5iaGN2aHdzYnJhcWJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzMzI3MTUsImV4cCI6MjA1OTkwODcxNX0.cgb6V0z8rro5GlVVrrAHzG3Mnt322BsOxuh_WNp0Gd0"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
